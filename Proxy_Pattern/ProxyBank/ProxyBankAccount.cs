5249b2bed0d25f9e324d9fae4fe4963713ae3597
Ramil <ramilteymurov264@gmail.com>
2023-12-14 13:28:34 +0400

    Patterns

--- Proxy_Pattern/ProxyBank/ProxyBankAccount.cs
+++ Proxy_Pattern/ProxyBank/ProxyBankAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,23 @@ namespace Proxy_Pattern.Proxy
     class ProxyBankAccount : IBankAccount
     {
         private BankAccount _bankAccount = new();
-
+        private string password = "mypassword123";
         public void AddAmount(double amount) 
         {
             _bankAccount.AddAmount(amount);
         }
         public void Spend(double amount) 
         {
-            _bankAccount.Spend(amount);
+            string inputPassword;
+            Console.Write("Enter password: ");
+            inputPassword = Console.ReadLine();
+
+            if (inputPassword != null && inputPassword == this.password) 
+            {
+                _bankAccount.Spend(amount);
+                return;
+            }
+            Console.WriteLine("Incorrect password!");
         }
 
         public double GetBalance() 
